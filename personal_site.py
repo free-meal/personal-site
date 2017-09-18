b50972816dc989d788839564aa259e781e4adcca
Matthew Ealy <ealy.matthew.d@gmail.com>
2017-09-18 09:29:23 -0400

    first draft

---
+++ personal_site.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/what-i-do')
+def projects_page():
+    return render_template("projects.html")
+
+
+@app.route('/')
+def about_page():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact_page():
+    return render_template("contact.html")
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/ico')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
